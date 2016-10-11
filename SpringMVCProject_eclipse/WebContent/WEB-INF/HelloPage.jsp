599d20c689e36d82a5e916fa8a7e9a73632c2797
Gyula <durdagyula@hotmail.com>
2016-10-11 18:40:41 +0200

    Annotations

--- SpringMVCProject_eclipse/WebContent/WEB-INF/HelloPage.jsp
+++ SpringMVCProject_eclipse/WebContent/WEB-INF/HelloPage.jsp
@@ -1,13 +1,6 @@
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
 <body>
-
-	<h1>Spring MVC app from the grand ECLIPSE</h1>
-	
-	<h2>${welcomeMessage}</h2>
-	
+	<h1>Spring MVC app from the grand ECLIPSE</h1>	
+	<h2>${msg}</h2>
 </body>
 </html>
--- SpringMVCProject_eclipse/src/com/controllers/HelloController.java
+++ SpringMVCProject_eclipse/src/com/controllers/HelloController.java
@@ -1,21 +1,18 @@
 package com.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController{
+@Controller
+public class HelloController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld(){
 		
-		ModelAndView modelandview = new ModelAndView("HelloPage");
-		modelandview.addObject("welcomeMessage", "Hi, this is my first eclipse nightmare");
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg", "hello world");
 		
-		return modelandview;
-	}
-	
+		return model;
+	}	
 }
