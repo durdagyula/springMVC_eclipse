e92882f5e3940e0ae3c3dcfc225511eb22bcb701
Durda <durda@gyula.com>
2016-10-11 16:54:01 +0200

    -init

---
+++ SpringMVCProject_eclipse/WebContent/WEB-INF/HelloPage.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>Spring MVC app from the grand ECLIPSE</h1>
+	
+	<h2>${welcomeMessage}</h2>
+	
+</body>
+</html>
---
+++ SpringMVCProject_eclipse/src/com/controllers/HelloController.java
@@ -0,0 +1,21 @@
+package com.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+		HttpServletResponse response) throws Exception {
+		
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("welcomeMessage", "Hi, this is my first eclipse nightmare");
+		
+		return modelandview;
+	}
+	
+}
