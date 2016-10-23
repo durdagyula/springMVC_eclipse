c751b8d889553af7aaa97eac082b828b82105d49
Gyula <durdagyula@hotmail.com>
2016-10-23 15:53:17 +0200

    @PathVariable, and MVC:annotations added.

--- SpringMVCProject_eclipse/src/com/controllers/HelloController.java
+++ SpringMVCProject_eclipse/src/com/controllers/HelloController.java
@@ -1,18 +1,36 @@
 package com.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-public class HelloController {
+@EnableWebMvc
+public class HelloController{
 
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld(){
+	@RequestMapping("/welcome/{countryName}/{userName}")
+	public ModelAndView helloWorld(@PathVariable Map<String,String> pathVars){
+		
+		String name = pathVars.get("userName");
+		String country = pathVars.get("countryName");
 		
 		ModelAndView model = new ModelAndView("HelloPage");
-		model.addObject("msg", "hello world");
+		model.addObject("msg", "hello " + name + "." + " You are from " + country);
 		
 		return model;
-	}	
+	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView hiWorld(){
+		
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg", "hi world");
+		
+		return model;
+	}
+	
 }
