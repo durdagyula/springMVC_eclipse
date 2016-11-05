22270159c3550b26e39aae3232220936455a2962
Gyula <durdagyula@hotmail.com>
2016-11-05 18:32:02 +0100

    ThemeResolver

--- SpringMVCProject_eclipse/WebContent/WEB-INF/AdmissionForm.jsp
+++ SpringMVCProject_eclipse/WebContent/WEB-INF/AdmissionForm.jsp
@@ -1,5 +1,16 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <html>
+<head>
+	<link rel="stylesheet" href="<spring:theme code='style'/>" type="text/css"/>
+</head>
 <body>
+		
+	<p>
+		<a href="/SpringMVCProject_eclipse/admissionForm.html?theme=default">default</a>
+		<a href="/SpringMVCProject_eclipse/admissionForm.html?theme=blue">blue</a>
+	</p>
+
 	<h1>USER ADMISSION FORM</h1>
 
 	<form action="/SpringMVCProject_eclipse/submitAdmissionForm.html" method="post">
--- SpringMVCProject_eclipse/src/com/controllers/UserAdmissionController.java
+++ SpringMVCProject_eclipse/src/com/controllers/UserAdmissionController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Controller
+@EnableWebMvc
 public class UserAdmissionController {
 	
 	@RequestMapping(value="/admissionForm.html", method = RequestMethod.GET)
